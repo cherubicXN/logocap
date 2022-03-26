d3b45b2fbd0d95653ea233715cfa322e318b8538
xn <xuenan199@gmail.com>
2022-03-26 15:04:42 +0800

    new file:   weights/download.sh 	modified:   README.md

---
+++ weights/download.sh
@@ -0,0 +1,7 @@
+wget https://github.com/cherubicXN/logocap/releases/download/checkpoints/imagenet.tar.gz
+wget https://github.com/cherubicXN/logocap/releases/download/checkpoints/logocap.tar.gz
+
+tar zxvf imagenet.tar.gz
+tar zxvf logocap.tar.gz
+
+rm imagenet.tar.gz logocap.tar.gz
